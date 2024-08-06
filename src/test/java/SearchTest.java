9101527e64faa64eb048afad4d2657aee56d12ec
Myrzatayev Temirlan <myzrataevtemirlan76@gmail.com>
2024-08-06 18:47:17 +0500

    hw1 qa duru - done

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -8,17 +9,19 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTest {
+    //открыть гугл и проверить есть ли там ютуб
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("YouTube").pressEnter();
         $("[id=search]").shouldHave(text("https://m.youtube.com"));
     }
-
+    //чекаем работает ли push and commit в мою ветку
     @Test
     void shouldOpen() {
         String date = "01.08.2024";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate localDate = LocalDate.parse(date, formatter);
     }
+
 }
